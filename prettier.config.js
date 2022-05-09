15d3ef727de1a08cb789d7969879386775ab6545
JacksonFA <jackson.f205@gmail.com>
2022-05-09 01:44:14 -0300

    add prettier and eslint

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+export const tabs = false
+export const tabWidth = 2
+export const semi = true
+export const singleQuote = true
+export const bracketSpacing = true
+export const trailingComma = 'es6'
+export const arrowParens = 'avoid'
+
